979b43ba39844f97efb370276c691e78e1a26b31
yungbrave <SergeyIlinR@gmail.com>
2023-08-14 23:53:31 +0300

    js fixed

---
+++ src/main/java/ru/herewego/bootJS/service/DbInitialData.java
@@ -0,0 +1,22 @@
+//package ru.herewego.bootJS.service;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//import javax.annotation.PostConstruct;
+//
+//@Component
+//public class DbInitialData {
+//
+//    private final UserService userService;
+//
+//    @Autowired
+//    public DbInitialData(UserService userService) {
+//        this.userService = userService;
+//    }
+//
+//    @PostConstruct
+//    public void setData() {
+//        userService.setInitData();
+//    }
+//}
